4f61ffacf0551495b59cbec33c31996fa71ac4bc
SamuelRosarioDev <samuelrosario.dev.com>
2025-04-03 16:45:41 -0300

    Componente Title

--- devbills-web/src/screens/Home/index.tsx
+++ devbills-web/src/screens/Home/index.tsx
@@ -1,16 +1,28 @@
 import { Button } from "../../components/Button";
 import { Logo } from "../../components/Logo";
-import { Header } from "./styles";
+import { Title } from "../../components/Title";
+import { Filters, Header, Main, Section } from "./styles";
 
 export function Home() {
     return (
-        <Header>
-            <Logo />
-            <div>
-                <Button>Nova transação</Button>
-                <Button>Nova categoria</Button>
-            </div>
-        </Header>
+        <>
+            <Header>
+                <Logo />
+                <div>
+                    <Button>Nova transação</Button>
+                    <Button>Nova categoria</Button>
+                </div>
+            </Header>
+
+            <Main>
+                <Section>
+                    <Filters>
+                        <Title title="Saldo" subtitle="Receitas e despensas no periodo" />
+                    </Filters>
+                </Section>
+            </Main>
+        </>
+
     );
 
 }
